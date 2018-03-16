60940d6f53e065f3c73213203746fd2e73a19012
ulgoon <ulgoon89@gmail.com>
2018-03-17 01:14:01 +0900

    add resources and handouts for day2

--- resources/sample/server.py
+++ resources/sample/server.py
@@ -7,5 +7,27 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/success')
+def success():
+    return render_template("success.html")
+
+@app.route('/orderinfo', methods=['POST','GET'])
+def orderinfo():
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            email = request.form['email']
+            address = request.form['address']
+            message = request.form['message']
+
+            msg = "All done, {}!".format(name)
+        except:
+            msg = "But, Order has problem.."
+        finally:
+            return render_template("success.html", msg=msg)
+    else:
+        return render_template("orderinfo.html")
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1')
